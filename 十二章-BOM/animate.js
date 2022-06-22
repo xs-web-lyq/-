01cc4704d4c5b93f70da19c47fdd0ee8b09855b3
xs-web-lyq <3235951041@qq.com>
2022-06-22 23:08:08 +0800

    移动端

--- 十二章-BOM/animate.js
+++ 十二章-BOM/animate.js
@@ -1,14 +1,13 @@
-function animate(obj,target){
+function animate(obj,target,callback){
     clearInterval(obj.timer);
-
-
     obj.timer = setInterval(function(){
         let step = (target - obj.offsetLeft) / 10 
         step = step > 0 ? Math.ceil(step) : Math.floor(step);
         if(obj.offsetLeft == target){
             clearInterval(obj.timer);
+            callback && callback();
         }
         obj.style.left = obj.offsetLeft + step + 'px';
-        console.log(obj.offsetLeft);
+
     },30)
 }
